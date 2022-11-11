38dbcc5d177b6992e6dda13d9c9298b0b1268134
William-Hourigan <117976431+William-Hourigan@users.noreply.github.com>
2022-11-11 11:11:29 +0100

    Aggiunto ES3_11-11-22 (William H 11:10)
    Carico il file dell'esercizio 3 perché prima l'avevo creato come file di testo.

---
+++ ES3_11-11-22.java
@@ -0,0 +1,22 @@
+class ES3 {
+    public static void main(String[] args) {
+        //  Dichiarazioni variabili da sommare
+        double num1 = 'b';
+        double num2 = 123.34;
+        int num3 = 1;
+
+        //  dichiarazione del numero finale e gli do la somma delle variabili dichiarate sopra e lo divido per 3
+        double numFinale = num1+num2+num3;
+        numFinale /= 3;
+
+        //  dichiarazione dei bool 
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        //  stampo tutto 
+        System.out.println((bool1==!bool2) + " " + numFinale + " Evviva");
+
+    }
+    //  Termine metodo main della classe ES3
+}
+//  Termine della classe ES3
